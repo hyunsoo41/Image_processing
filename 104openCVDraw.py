3ee4d783873e46482b00fb7c18d1a6348870b260
HyunSoo Seo <81602079+hyunsoo41@users.noreply.github.com>
2021-09-10 19:23:04 +0900

    Add files via upload

---
+++ 104openCVDraw.py
@@ -0,0 +1,33 @@
+import cv2
+
+print("OpenCV version:")
+print(cv2.__version__)
+
+img = cv2.imread("nomadProgramerIcon.png")
+print("width: {} pixels".format(img.shape[1]))
+print("height: {} pixels".format(img.shape[0]))
+print("channels: {}".format(img.shape[2]))
+
+cv2.imshow("nomadProgramer", img)
+
+(b, g, r) = img[0, 0]
+print("Pixel at (0, 0) - Red: {}, Green: {}, Blue: {}".format(r,
+g, b))
+
+dot = img[50:100, 50:100]
+
+img[50:100, 50:100] = (0, 0, 255)
+
+cv2.rectangle(img, (150, 50), (200, 100), (0, 255, 0), 5)  # 픽셀 시작, 끝, 색상, 선의 굵기 
+
+cv2.circle(img, (275, 75), 25, (0, 255, 255), -1)  # 중심의 위치, 반지름, 색상, -1->전체 채우기 
+
+cv2.line(img, (350, 100), (400, 100), (255, 0, 0), 5)  # 픽셀 시작, 끝, 색상, 굵기 
+
+cv2.putText(img, 'creApple', (450, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 4)
+# 픽셀 시작(좌측 하단), 폰트, 폰트의 크기, 색상, 폰트의 굵기 
+
+cv2.imshow("nomadProgramer - draw", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
